fb36e9e0091351ac94a203c56bc7dbd552cbe4de
Joe Bullard <joekbullard@gmail.com>
2025-02-06 21:30:30 +0000

    Add test JsonResponse view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from api.models import Board, Reading
 
-# Create your views here.
+def boards_view(request):
+    boards = Board.objects.all()
+    return JsonResponse(boards)
+
+
