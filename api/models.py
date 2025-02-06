091223d96ab9416782343e1976927ff488651954
Joe Bullard <joekbullard@gmail.com>
2025-02-06 21:24:56 +0000

    Add board and reading model

---
+++ api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+
+class Board(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=40)
+    uid = models.CharField(max_length=16, unique=True)
+
+
+class Reading(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="readings")
+    timestamp = models.DateTimeField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    pressure = models.FloatField()
+    luminance = models.FloatField()
+    moisture_a = models.FloatField()
+    moisture_b = models.FloatField()
+    moisture_c = models.FloatField()
