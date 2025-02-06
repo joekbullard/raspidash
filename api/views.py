34554b39d84cd95863e9e8b9b70394d5b7b591a6
Joe Bullard <joekbullard@gmail.com>
2025-02-06 21:57:24 +0000

    Add urls and views for boards and readings endpoints

--- api/views.py
+++ api/views.py
@@ -1,8 +1,18 @@
-from django.http import JsonResponse
+from django.core import serializers
+from django.http import HttpResponse
 from api.models import Board, Reading
 
-def boards_view(request):
-    boards = Board.objects.all()
-    return JsonResponse(boards)
+def boards(request):
 
+    if request.method == 'GET':
+        boards = Board.objects.all()
+        data = serializers.serialize('json', boards)
+        return HttpResponse(data, content_type='application/json')
+    
 
+def readings(request, board_id):
+
+    if request.method == 'GET':
+        readings = Reading.objects.filter(board_id=board_id)
+        data = serializers.serialize('json', readings)
+        return HttpResponse(data, content_type='application/json')
