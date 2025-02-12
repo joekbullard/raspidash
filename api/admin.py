6de4b3821fa74ae418fc742554c3fdc72fb5ef24
Joe Bullard <joekbullard@gmail.com>
2025-02-12 22:13:26 +0000

    Adds MoistsureSensor model to admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from api.models import Board, Reading
+from api.models import Board, Reading, MoistureSensor
 
 admin.site.register(Board)
 admin.site.register(Reading)
+admin.site.register(MoistureSensor)
