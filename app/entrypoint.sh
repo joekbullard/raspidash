4b9793a16a5d1d79b0d6f209218e62ab11afcaff
Joe Bullard <joekbullard@gmail.com>
2025-02-22 22:01:10 +0000

    remove unused dockerize command from Dockerfile

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# dockerize -wait tcp://"${POSTGRES_HOST}":"${POSTGRES_PORT}" -timeout 0.1s
-
 until nc -z "${POSTGRES_HOST}" "${POSTGRES_PORT}"; do
   echo "Waiting for database..."
   sleep 1
