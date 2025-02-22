7c766fe303c22a0062984b2c18f7bb66f464be38
Joe Bullard <joekbullard@gmail.com>
2025-02-22 23:56:33 +0000

    remove dashboard and board routes

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from api.views import boards, board_detail, submit_reading
 
 urlpatterns = [
-    path('boards/', boards),
-    path('dashboard/', board_detail),
-    path('dashboard/<int:board_id>/', board_detail, name="board_detail"),
+    path('', board_detail),
+    path('<int:board_id>/', board_detail, name="board_detail"),
     path('submit-reading/', submit_reading)
 ]
