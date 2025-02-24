d5ae85f09991c1d9dbc8e47b3666d4cdfc9276b1
Joe Bullard <joekbullard@gmail.com>
2025-02-24 21:42:00 +0000

    Replaces os.environ.get with os.getenv

--- app/config/settings.py
+++ app/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "1"
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
