dcee22097d3200f20c5a9ecfa071d9ade424646c
Joe Bullard <joekbullard@gmail.com>
2025-02-26 17:11:54 +0000

    Fix DEBUG typo and remove admin from url imports

--- app/config/settings.py
+++ app/config/settings.py
@@ -28,7 +28,7 @@ SU_PASSWORD = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == "1"
+DEBUG = os.getenv('DJANGO_DEBUG') == "1"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
